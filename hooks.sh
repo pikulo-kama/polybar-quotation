c2a85232cd75015f423c769e276b7697f5db081f
Arthur Drabazha <curtisconnor818@gmail.com>
2020-10-19 12:02:08 +0300

    hooks.sh: reformat hooks

---
+++ hooks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+if [ $1 = "1" ]; then
+
+	/home/`whoami`/.config/polybar/scripts/quotations/quot.py --next-line
+	polybar-msg -p "$(pgrep -f "polybar example")" hook quotation 1
+
+elif [ $1 = "2" ]; then
+	polybar-msg -p "$(pgrep -f "polybar example")" hook quotation 2
+
+fi
