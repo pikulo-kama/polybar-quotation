7c1c0080674a30a2c9070891ff4604c5703a0d28
Arthur Drabazha <curtisconnor818@gmail.com>
2020-10-19 13:33:30 +0300

    Initial commit

--- hooks.sh
+++ hooks.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+bar_name="example"
 
 if [ $1 = "1" ]; then
 
 	/home/`whoami`/.config/polybar/scripts/polybar-quotation/quot.py --next-line
-	polybar-msg -p "$(pgrep -f "polybar example")" hook quotation 1
+	polybar-msg -p "$(pgrep -f "polybar "$bar_name)" hook quotation 1
 
 elif [ $1 = "2" ]; then
-	polybar-msg -p "$(pgrep -f "polybar example")" hook quotation 2
+	polybar-msg -p "$(pgrep -f "polybar "$bar_name)" hook quotation 2
 
 fi
