36023c8218faffebe16f2b4341ddf06fbe1bf67f
Arthur Drabazha <curtisconnor818@gmail.com>
2020-10-20 04:36:39 +0300

    Add fetching of bar name from config

--- hooks.sh
+++ hooks.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-bar_name="example"
+workdir="/home/`whoami`/.config/polybar/scripts/polybar-quotation"
+bar_name=`jq ".bar_name" $workdir/conf.json`
+bar_name=`echo $bar_name | cut -c2-$((${#bar_name}-1))`
+
 
 if [ $1 = "1" ]; then
 
-	/home/`whoami`/.config/polybar/scripts/polybar-quotation/quot.py --next-line
-	polybar-msg -p "$(pgrep -f "polybar "$bar_name)" hook quotation 1
+	$workdir/quot.py --next-line
+	polybar-msg -p `pgrep -f "polybar $bar_name"` hook quotation 1
 
 elif [ $1 = "2" ]; then
 	polybar-msg -p "$(pgrep -f "polybar "$bar_name)" hook quotation 2
